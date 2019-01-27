40b2cd65b0cae0b1ad8cfa7c1f9ac6785202f59d
Andrei V <andrei@ptaxa.net>
2019-01-27 22:28:44 +0300

    + basic docs

--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,43 @@
+//! # enum_bits [![pipeline status](https://gitlab.com/PtaxLaine/enum-bits-rs/badges/master/pipeline.svg)](https://gitlab.com/PtaxLaine/enum-bits-rs/commits/master)
+//!
+//!
+//! ## Documentation
+//!
+//! ...
+//!
+//! ## Usage
+//!
+//! Add dependency into `Cargo.toml` file:
+//!
+//! ```toml
+//! [dependencies]
+//! enum_bits = "0.1"
+//! ```
+//!
+//! Import derive macros to you source with `use enum_bits::EnumBits;`
+//!
+//! Add explicit type defination for you enum `#[repr(i32)]`
+//!
+//! Add derive to you enum `#[derive(EnumBits)]`
+//!
+//! ## Example
+//!
+//! ```rust
+//! use enum_bits::EnumBits;
+//!
+//! #[repr(i64)]
+//! #[derive(EnumBits)]
+//! #[derive(Debug, PartialEq)] // assert_eq required
+//! enum TestEnum {
+//!     Foo, Bar, Biz=-57
+//! }
+//!
+//! assert_eq!(Some(TestEnum::Foo), TestEnum::read_i64(0));
+//! assert_eq!(Some(TestEnum::Bar), TestEnum::read_i64(1));
+//! assert_eq!(Some(TestEnum::Biz), TestEnum::read_i64(-57));
+//! assert_eq!(None, TestEnum::read_i64(2));
+//! ```
+
 pub use enum_bits_derive::EnumBits;
 pub use lazy_static::lazy_static;
 
